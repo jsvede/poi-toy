b4d68c7b0335f0c26042306ba94a27fd83d29eef
Jon Svede <jsvede@yahoo.com>
2011-06-29 07:15:23 -0600

    Adding AddUdfWizardPage.

---
+++ com.loquatic.xls.gui/src/com/loquatic/xls/gui/wizards/AddUdfWizard.java
@@ -0,0 +1,19 @@
+package com.loquatic.xls.gui.wizards;
+
+import org.eclipse.jface.wizard.Wizard;
+
+public class AddUdfWizard extends Wizard {
+	
+	
+	public AddUdfWizard() {
+		super();
+		
+	}
+
+	@Override
+	public boolean performFinish() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
