7e4d606e57beea3a0a5c2dfb558dade24a4d4d04
Jon Svede <jsvede@yahoo.com>
2011-06-29 07:05:01 -0600

    Adding new class for OpenAddUdfWizard command.

---
+++ com.loquatic.xls.gui/src/com/loquatic/xls/gui/commands/OpenAddUdfWizard.java
@@ -0,0 +1,18 @@
+package com.loquatic.xls.gui.commands;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+
+public class OpenAddUdfWizard extends AbstractHandler implements IHandler {
+
+	public final static String ID = "com.loquatic.xls.gui.command.openAddUdfWizard" ;
+	
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
