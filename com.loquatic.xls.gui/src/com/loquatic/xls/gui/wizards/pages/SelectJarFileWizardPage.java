3c53803f2cdc3065821b56c98c732e129661d1fc
Jon Svede <jsvede@yahoo.com>
2011-06-29 07:15:44 -0600

    Add SelectJarFileWizardPage to repo

---
+++ com.loquatic.xls.gui/src/com/loquatic/xls/gui/wizards/pages/SelectJarFileWizardPage.java
@@ -0,0 +1,20 @@
+package com.loquatic.xls.gui.wizards.pages;
+
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.widgets.Composite;
+
+public class SelectJarFileWizardPage extends WizardPage {
+
+	
+	public SelectJarFileWizardPage(String pageName) {
+		super(pageName);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
