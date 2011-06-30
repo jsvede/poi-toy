d6dce46ee332d8594ed212d0f43ff2821cbb9825
Jon Svede <jsvede@yahoo.com>
2011-06-30 07:46:35 -0600

    Updated the file selection wizard page.

--- com.loquatic.xls.gui/src/com/loquatic/xls/gui/wizards/pages/SelectJarFileWizardPage.java
+++ com.loquatic.xls.gui/src/com/loquatic/xls/gui/wizards/pages/SelectJarFileWizardPage.java
@@ -1,20 +1,61 @@
 package com.loquatic.xls.gui.wizards.pages;
 
 import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 
 public class SelectJarFileWizardPage extends WizardPage {
 
+	private Composite composite ;
+		
+	private Text selectFileNameTxt ;
+	private Combo choosenFilesCbo ;
 	
 	public SelectJarFileWizardPage(String pageName) {
 		super(pageName);
-		// TODO Auto-generated constructor stub
+		setTitle( Messages.SelectJarFileWizardPage_Title ) ;
+		setDescription( Messages.SelectJarFileWizardPage_Description ) ;
 	}
 
 	@Override
 	public void createControl(Composite parent) {
-		// TODO Auto-generated method stub
-
+		composite = new Composite( parent, SWT.NULL ) ; 
+		
+		FormLayout layout = new FormLayout() ;
+		
+		composite.setLayout( layout ) ;
+		
+		FormData formData = new FormData() ;
+		formData.top = new FormAttachment( 0, 10 ) ;
+		formData.left = new FormAttachment( 0, 10 ) ;
+	
+		Label selectFileLbl = new Label( composite, SWT.NONE ) ;
+		selectFileLbl.setText( Messages.SelectJarFileWizardPage_Label_Select_File ) ;
+		selectFileLbl.setLayoutData( formData ) ;
+		
+		selectFileNameTxt = new Text( composite, SWT.BORDER ) ;
+		
+		formData = new FormData() ;
+		formData.top = new FormAttachment( 0, 10 ) ;
+		formData.left = new FormAttachment( selectFileLbl, 10 ) ;
+		selectFileNameTxt.setLayoutData( formData ) ;
+	
+		Button chooseFileBtn = new Button( composite, SWT.PUSH ) ;
+		chooseFileBtn.setText( Messages.SelectJarFileWizardPage_Button_Select_File_Label ) ;
+		formData = new FormData() ;
+		formData.top = new FormAttachment( 0, 10 ) ;
+		formData.left = new FormAttachment( selectFileNameTxt, 10 ) ;
+		chooseFileBtn.setLayoutData( formData ) ;
+//		chooseFileBtn.add
+		
+		setControl( composite ) ;
 	}
 
 }
