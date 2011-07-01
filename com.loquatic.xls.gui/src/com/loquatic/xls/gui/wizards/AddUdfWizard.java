a8e33b8c5ed74d714ae1fb2310f8494bb33e8350
Jon Svede <jsvede@www.pkt.pl.jsvedelt.corp.localmatters.com>
2011-07-01 06:57:12 -0600

    Added the SelectJarFileWizardPage.

--- com.loquatic.xls.gui/src/com/loquatic/xls/gui/wizards/AddUdfWizard.java
+++ com.loquatic.xls.gui/src/com/loquatic/xls/gui/wizards/AddUdfWizard.java
@@ -2,18 +2,26 @@ package com.loquatic.xls.gui.wizards;
 
 import org.eclipse.jface.wizard.Wizard;
 
+import com.loquatic.xls.gui.wizards.pages.SelectJarFileWizardPage;
+
 public class AddUdfWizard extends Wizard {
 	
+	
+	private SelectJarFileWizardPage selectJarFile  = new SelectJarFileWizardPage() ;
 	
 	public AddUdfWizard() {
 		super();
 		
 	}
+	
+	public void addPages() {
+		addPage( selectJarFile  ) ;
+	}
 
 	@Override
 	public boolean performFinish() {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 }
