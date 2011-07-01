1ac1c932b935e234065dc100eb2b26a8f5b52df5
Jon Svede <jsvede@yahoo.com>
2011-07-01 16:09:05 -0600

    Updated wizard page to be able to finish when the use selects a valid file.

--- com.loquatic.xls.gui/src/com/loquatic/xls/gui/wizards/pages/SelectJarFileWizardPage.java
+++ com.loquatic.xls.gui/src/com/loquatic/xls/gui/wizards/pages/SelectJarFileWizardPage.java
@@ -1,13 +1,18 @@
 package com.loquatic.xls.gui.wizards.pages;
 
+import java.io.File;
+
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
@@ -30,6 +35,8 @@ public class SelectJarFileWizardPage extends WizardPage {
 
 	@Override
 	public void createControl(Composite parent) {
+		
+		setPageComplete( false ) ;
 		composite = new Composite( parent, SWT.NULL ) ; 
 		
 		FormLayout layout = new FormLayout() ;
@@ -60,7 +67,41 @@ public class SelectJarFileWizardPage extends WizardPage {
 		formData.top = new FormAttachment( 0, 10 ) ;
 		formData.left = new FormAttachment( selectFileNameTxt, 10 ) ;
 		chooseFileBtn.setLayoutData( formData ) ;
-//		chooseFileBtn.add
+		chooseFileBtn.addSelectionListener( 
+			new SelectionListener() {
+
+				@Override
+				public void widgetSelected(SelectionEvent e) {
+					String[] extensionFilters = { "*.jar" };
+					FileDialog fileDialog = new FileDialog( composite.getShell(), SWT.OPEN ) ;
+					fileDialog.setFilterExtensions( extensionFilters ) ;
+					String selectedJar = fileDialog.open() ;
+					
+					if( selectedJar != null  ) {
+						selectFileNameTxt.setText( selectedJar ) ;
+						File selectedJarFile = new File( selectedJar ) ;
+						if( selectedJarFile.exists() && 
+								selectedJarFile.canRead() ) {
+							setPageComplete( true ) ;
+						} else {
+							setErrorMessage( Messages.SelectJarFileWizardPage_CannotReadFile_Message ) ;
+						}
+					} else {
+						setErrorMessage( Messages.SelectJarFileWizardPage_InvalidFile_Message ) ;
+					}
+					
+					
+					
+				}
+
+				@Override
+				public void widgetDefaultSelected(SelectionEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+			} ) ;
+		
 		
 		setControl( composite ) ;
 	}
