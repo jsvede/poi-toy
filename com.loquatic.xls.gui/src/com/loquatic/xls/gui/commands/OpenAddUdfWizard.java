5b87870bf20a3de450f5696ef5339abc0a790b9c
Jon Svede <jsvede@www.pkt.pl.jsvedelt.corp.localmatters.com>
2011-07-01 06:56:34 -0600

    added code to open the AddUdfWizard wizard.

--- com.loquatic.xls.gui/src/com/loquatic/xls/gui/commands/OpenAddUdfWizard.java
+++ com.loquatic.xls.gui/src/com/loquatic/xls/gui/commands/OpenAddUdfWizard.java
@@ -4,6 +4,10 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import com.loquatic.xls.gui.wizards.AddUdfWizard;
 
 public class OpenAddUdfWizard extends AbstractHandler implements IHandler {
 
@@ -11,7 +15,13 @@ public class OpenAddUdfWizard extends AbstractHandler implements IHandler {
 	
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		// TODO Auto-generated method stub
+		AddUdfWizard addUdfWizard = new AddUdfWizard() ;
+		
+		WizardDialog dialog = new WizardDialog(HandlerUtil
+				.getActiveShell(event), addUdfWizard ) ;
+		dialog.open();
+
+		
 		return null;
 	}
 
